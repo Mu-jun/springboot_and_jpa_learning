2023a823eb7fc27d9af5e0da02216284d1a8c111
Mu-jun <suo24691@gmail.com>
2023-04-23 21:35:26 +0900

    주문 검색 기능 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,17 +1,31 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.domain.Order;
-import lombok.RequiredArgsConstructor;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+        this.jpaQueryFactory = new JPAQueryFactory(em);
+    }
+
+    QOrder order = QOrder.order;
+    QMember member = QMember.member;
+
+
 
     public void save(Order order) {
         em.persist(order);
@@ -21,5 +35,25 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch)
+    public List<Order> findAll(OrderSearch orderSearch) {
+
+        return jpaQueryFactory
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(
+                        nameLike(orderSearch.getMemberName()),
+                        statusEq(orderSearch.getOrderStatus())
+                )
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        return memberName == null ? null : order.member.name.contains(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus orderStatus) {
+        return orderStatus == null ? null : order.status.eq(orderStatus);
+    }
 }
